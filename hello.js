fb59820cf068a216a54bd16b0d89a184c32f7ea6
clear clear <biolink47@gmail.com>
2023-08-04 16:28:39 +0700

    d

---
+++ hello.js
@@ -0,0 +1,7 @@
+
+const bulat = document.querySelector('.gak');
+const dark = document.querySelector('.hara');
+bulat.addEventListener('click', function(){
+    bulat.classList.toggle('move')
+    dark.classList.toggle('dark')
+})
